a8fb2815c4b650c4597cf578177dcd43bc8bdb98
Andrei Kislichenko <android.2net@gmail.com>
2024-06-02 22:52:45 -0400

    fix issue #18

--- lib/relaton_doi/crossref.rb
+++ lib/relaton_doi/crossref.rb
@@ -2,6 +2,10 @@ module RelatonDoi
   module Crossref
     extend self
 
+    HEADER = {
+      "User-Agent" => "Relaton/RelatonDoi (https://www.relaton.org/guides/doi/; mailto:open.source@ribose.com)"
+    }.freeze
+
     #
     # Get a document by DOI from the CrossRef API.
     #
@@ -15,11 +19,13 @@ module RelatonDoi
       Util.warn "(#{doi}) Fetching from search.crossref.org ..."
       id = doi.sub(%r{^doi:}, "")
       message = get_by_id id
-      Util.warn "(#{doi}) Found: `#{message['DOI']}`"
-      Parser.parse message
-    rescue Serrano::NotFound
-      Util.warn "(#{doi}) Not found."
-      nil
+      if message
+        Util.warn "(#{doi}) Found: `#{message['DOI']}`"
+        Parser.parse message
+      else
+        Util.warn("(#{doi}) Not found.")
+        nil
+      end
     end
 
     #
@@ -30,8 +36,24 @@ module RelatonDoi
     # @return [Hash] The document.
     #
     def get_by_id(id)
-      resp = Serrano.works ids: id
-      resp[0]["message"]
+      # resp = Serrano.works ids: id
+      n = 0
+      url = "https://api.crossref.org/works/#{ERB::Util.url_encode(id)}"
+      loop do
+        resp = Faraday.get url, nil, HEADER
+        case resp.status
+        when 200
+          work = JSON.parse resp.body
+          return work["message"] if work["status"] == "ok"
+        when 404 then return nil
+        end
+
+        if n > 1
+          raise RelatonBib::RequestError, "Crossref error: #{resp.body}"
+        end
+        n += 1
+        sleep resp.headers["x-rate-limit-interval"].to_i * n
+      end
     end
   end
 end
